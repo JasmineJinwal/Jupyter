67dad87087133947962d99e04736a2f9f45fbe0e
Jasmine Jinwal <41662107+JasmineJinwal@users.noreply.github.com>
2018-08-24 11:59:27 +0530

    Add files via upload

---
+++ Numpy_array.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 10 11:21:08 2018
+
+@author: bpm
+"""
+
+import numpy as np
+
+a = np.array([1,2])
+b = np.array([2,4,1.7])
+c = np.array( [ [1,2], [3,4] ], dtype=complex )
+c = np.zeros((4,8))
+d = np.ones( (2,3,4), dtype=np.int16 )
+e = np.linspace( 0, 2, 20 )  
+print(np.arange(10000).reshape(100,100))
+a = np.array([1,2,3,4])
+b = np.arange(4)
+
